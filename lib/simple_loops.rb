562eb41acbf76bfab3ff451f5a594bb29a45bd84
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-04 15:38:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,11 +22,13 @@ def output_array(cheer = ["Hoo!", "Hoo!", "Hoo!", "Hoosiers!"])
   end
 end
 
-def return_string_array(array = ["You", "Got", "Real", "Problems"])
+def return_string_array(array)
   count = 0
+  new_array = []
   while count < array.length do
-    array[count].to_s
+    new_array.push(array[count].to_s)
     count += 1
   end
+  new array
 end
   
