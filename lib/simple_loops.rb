e7f1af6a8ce2ffb520e63c74827fffc4286b0388
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-04 15:15:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,10 +5,3 @@ def loop_message_five_times(message = "Hello World")
   counter += 1
 end
 end
-
-def loop_message_n_times(message = "Hello World", n = 5)
-  while n <= 5 do
-  puts message
-  n += 1
-end
-end
